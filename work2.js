b4247f78a7b875f56395a168fca9a7e3403a2a09
Lipskiy Michael <miha1799@icloud.com>
2025-03-24 08:13:35 +0200

    Створена функція

---
+++ work2.js
@@ -0,0 +1,18 @@
+"use strict"
+
+
+function pow(x, y) {
+    let result = 1; 
+        for (let i = 0; i < y; i++) { 
+            result *= x;
+        }
+
+    return result;
+}
+    const resultAll = pow(4,3);
+    console.log(resultAll)
+     
+
+
+
+
