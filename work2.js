ded69aabee4f26069324378dcf42093afac216b7
Lipskiy Michael <miha1799@icloud.com>
2025-09-09 16:52:51 +0300

    Update work2.js

--- work2.js
+++ work2.js
@@ -13,6 +13,44 @@ function pow(x, y) {
     console.log(resultAll)
      
 
+function SayHello() {
+    console.log("Привіт Javascript")
+}
+SayHello()
+SayHello()
+
+function Greet(name) {
+    console.log(`Привіт, ${name}`)
+}
+Greet("Валерій")
+
+
+function sum(a,b) {
+   return (a + b)
+}
+let result = sum(5, 5)
+let result2 = sum(200, 345)
+console.log(result)
+console.log(result2)
+
+
+function isAdult(age) {
+    if (age >= 18) {
+    return "Повнолітній"; 
+    } else {
+         return "Дитина"}
+    }
+const result = isAdult(15)
+console.log(result)
+
+
+function printName(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log(i)
+    }
+}
+printName(5)
+
 
 
 
