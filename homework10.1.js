bd05c65df25ed064fff86ade456baea2b56e3a40
Lipskiy Michael <miha1799@icloud.com>
2025-09-30 19:38:48 +0300

    Update homework10.1.js

--- homework10.1.js
+++ homework10.1.js
@@ -20,4 +20,4 @@ const validEmail = /^[A-Za-z0-9]+\.{1}[A-Za-z0-9]+@(gmail\.com|yahoo\.com)$/;
 const ValidatedArr = arr.filter(user => validEmail.test(user.email));
 console.log(ValidatedArr)
 
-var re = /^a-zA-Z
+
